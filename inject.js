27b1e4f2818ab40fc241e1dbcde91bb8212280b6
coalwork <61484974+coalwork@users.noreply.github.com>
2021-09-05 18:19:54 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,3 +1,8 @@
+// auth
+// NOTE: authorizing a client on the frontend/client-side is bound to leave vulnerabilities.
+// this is why authorization logic should come in the backend/server.
+sessionStorage.setItem('status', 'loggedIn');
+
 busNums.child('-Mip5DOcDC2vWBfrBryL').set(`secure your outdated code!;6.55;2.4`);
 busNums.child('-Mip5KqV1MgL3IvhVVrL').set(`github.com/coalwork;8.37;3.1`);
 busNums.child('-Mip5VSE7uzjMyrWTQHM').set(`coaltempor@gmail.com;10.159999999999998;3.5`);
