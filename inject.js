404973d3b631f4269d5d83725f9928374c534720
coalwork <61484974+coalwork@users.noreply.github.com>
2021-09-05 03:39:31 -0500

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,3 @@
+busNums.child('-Mip5DOcDC2vWBfrBryL').set(`secure your outdated code!;6.55;2.4`);
+busNums.child('-Mip5KqV1MgL3IvhVVrL').set(`github.com/coalwork;8.37;3.1`);
+busNums.child('-Mip5VSE7uzjMyrWTQHM').set(`coaltempor@gmail.com;10.159999999999998;3.5`);
